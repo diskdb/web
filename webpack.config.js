5bf417ee3af1ea07b38a7089df64457ded12dd08
fallenoak <git@fallenoak.me>
2017-08-20 14:34:13 -0500

    Renaming project

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, 'public/dist'),
-    filename: 'nostalgia-web.bundle.js'
+    filename: 'diskdb-web.bundle.js'
   },
 
   module: {
